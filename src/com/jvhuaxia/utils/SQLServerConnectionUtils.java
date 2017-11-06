0836c2fd0b74513a23fee57e5d6ef70806b0cf7b
jvhuaxia <jvhuaxia@qq.com>
2017-11-06 16:29:46 +0800

    Add SQLServerConnectionUtils

--- src/com/jvhuaxia/utils/SQLServerConnectionUtils.java
+++ src/com/jvhuaxia/utils/SQLServerConnectionUtils.java
@@ -7,9 +7,8 @@ import java.sql.SQLException;
 public class SQLServerConnectionUtils {
 	public static Connection getConnection(String host, String port, String dbName, String userName, String userPwd)
 			throws ClassNotFoundException, SQLException {
-		String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 		String dbURL = "jdbc:sqlserver://" + host + ":" + port + ";databaseName=" + dbName;
-		Class.forName(driverName);
 		Connection dbConn = DriverManager.getConnection(dbURL, userName, userPwd);
 		return dbConn;
 	}
