64a9cb30b7fa01fba5b08537f883f89aeb1973f6
jvhuaxia <jvhuaxia@qq.com>
2017-11-11 21:08:25 +0800

    iii

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,9 @@
 import com.jvhuaxia.utils.MyUtils;
 
-public class Test {
-	public static void main(String[] args) throws Throwable {
-		MyUtils.downloadFile("http://racaljk.github.io/verbatimAnim/public/music1.mp3", "D:\\v.mp3");
+public class Test
+{
+	public static void main(String[] args) throws Throwable
+	{
+		MyUtils.downloadFile("http://bbs.aimoxiang.com/zr/zr.html", "D:\\zr.html");
 	}
 }
--- src/com/jvhuaxia/utils/MyUtils.java
+++ src/com/jvhuaxia/utils/MyUtils.java
@@ -7,15 +7,18 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MyUtils {
+import javax.swing.plaf.InputMapUIResource;
 
-	
-	
-	public static void downloadFile(String url, String path) throws IOException {
+public class MyUtils
+{
+
+	public static void downloadFile(String url, String path) throws IOException
+	{
 		downloadFile(url, new File(path));
 	}
 
-	public static void downloadFile(String url, File file) throws IOException {
+	public static void downloadFile(String url, File file) throws IOException
+	{
 		HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(url).openConnection();
 		httpURLConnection.connect();
 		InputStream is = httpURLConnection.getInputStream();
@@ -24,38 +27,56 @@ public class MyUtils {
 		httpURLConnection.disconnect();
 	}
 
-	public static void writeInputStream2File(InputStream is, File file, int buffLength) throws IOException {
+	public static void writeInputStream2File(InputStream is, File file, int buffLength) throws IOException
+	{
 		byte[] buff = new byte[buffLength];
 		int readWhere = -1;
 		FileOutputStream fos = new FileOutputStream(file);
-		while ((readWhere = is.read(buff)) != -1) {
+		while ((readWhere = is.read(buff)) != -1)
+		{
 			fos.write(buff, 0, readWhere);
 		}
 		fos.close();
 		is.close();
 	}
 
-	public static void writeInputStream2File(InputStream is, String path) throws IOException {
+	public static void writeInputStream2File(InputStream is, String path) throws IOException
+	{
 		writeInputStream2File(is, new File(path), 10240);
 	}
 
-	public static void writeInputStream2File(InputStream is, File file) throws IOException {
+	public static void writeInputStream2File(InputStream is, File file) throws IOException
+	{
 		writeInputStream2File(is, file, 10240);
 	}
 
-	public static void writrString2File(File file, String string, String charset, boolean isAppend) throws IOException {
+	public static void writrString2File(File file, String string, String charset, boolean isAppend) throws IOException
+	{
 		FileOutputStream fos = new FileOutputStream(file, isAppend);
 		fos.write(string.getBytes(charset));
 		fos.close();
 	}
 
-	public static void writeString2File(String filePath, String string) throws IOException {
+	public static void writeString2File(String filePath, String string) throws IOException
+	{
 		writrString2File(new File(filePath), string, "UTF-8", false);
 	}
 
-	private static int bytes2int(byte[] bs) {
+	public static int bytes2int(byte[] bs)
+	{
 		int value;
 		value = (bs[0] & 0xFF) | ((bs[1] & 0xFF) << 8) | ((bs[2] & 0xFF) << 16) | ((bs[3] & 0xFF) << 24);
 		return value;
+	}
+
+	public static boolean isNumber(String inNUmberString)
+	{
+		for (int i = 0; i < inNUmberString.length(); i++)
+		{
+			char c = inNUmberString.charAt(0);
+			if (!(c <= 48 || c >= 57))
+				return false;
+		}
+		return true;
 	}
 }
